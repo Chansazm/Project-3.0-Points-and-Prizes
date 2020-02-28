8fc560c3018426707306edca3eeaa4b89da0d4b6
Chansa Chishimba <chansazm@yahoo.co.uk>
2020-02-28 06:40:25 -0600

    Add files via upload

---
+++ points and prizes.py
@@ -0,0 +1,12 @@
+points = 189
+
+if points <= 50:
+    result = "Wodden rabbit"
+elif points <= 150:
+    result = "no prize"
+elif points <= 180:
+    result = "Wafer-thin mint"
+else:
+    points <=200
+    result = "Penguin"
+print(result)
